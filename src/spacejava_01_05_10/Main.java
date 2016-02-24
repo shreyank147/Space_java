e382fb542d4e6e28ad8baeca4ba817bec0dc0e29
Prayag <ppatel96@hawk.iit.edu>
2016-02-24 11:18:27 -0600

    Made change line 51

--- src/spacejava_01_05_10/Main.java
+++ src/spacejava_01_05_10/Main.java
@@ -48,7 +48,7 @@ public class Main {
        logger.addHandler(logFile);
        logger.log(Level.INFO, "::::::::::::::::::Init Logging::::::::::::::::::");
        Random rand = new Random();      
-
+//Shreyank is stupid little puto
 
    Planet jupiter = new Planet(36,(rand.nextInt(POSITIVE_Y_LIMIT - NEGATIVE_Y_LIMIT + 1) + NEGATIVE_Y_LIMIT),1.5,"Juptier",2000,6000,100,100,100,100,200,10,100);
    Planet earth = new Planet (12,(rand.nextInt(POSITIVE_Y_LIMIT - NEGATIVE_Y_LIMIT + 1) + NEGATIVE_Y_LIMIT),1.0,"Earth",25000,10000,1000,1000,1000,1200,50,100,100);
